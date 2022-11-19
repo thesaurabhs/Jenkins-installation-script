28cbfc73e963108cecba1225b050f0f60aaef9ee
thesaurabhs <112174017+thesaurabhs@users.noreply.github.com>
2022-11-19 15:40:25 +0530

    Update Jenkins_installation.sh

--- Jenkins_installation.sh
+++ Jenkins_installation.sh
@@ -1,7 +1,8 @@
 //Jenkins Installation
 
 sudo apt-get update
-sudo apt install openjdk-8-jdk -y
+sudo apt install ca-certificates
+sudo apt install openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/> /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
